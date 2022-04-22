b1fdd8d6db6d20573cb9eb586dcc038152925239
Fabio Vaz <fabiohenriquevaz@hotmail.com>
2022-04-22 14:22:37 -0300

    Up projeto

---
+++ AtividadeAval01/Controllers/ConvidadosController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AtividadeAval01.Models;
+namespace AtividadeAval01.Controllers
+{
+    public class ConvidadosController : Controller
+    {
+        private static IList<Convidado> convidados = new List<Convidado>() {
+            new Convidado()
+            {
+                ConvidadoId = 1, Name = "Fabio", EMail = "fabiohv@gmail.com", Comparecimento = null, Telefone = "(35)9 9867-2893"
+            }
+        };
+        public IActionResult Index()
+        {
+            return View(convidados);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Convidado conv)
+        {
+            convidados.Add(conv);
+            conv.ConvidadoId = convidados.Select(m => m.ConvidadoId).Max() + 1;
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            return View(convidados.Where(m => m.ConvidadoId == id).First());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Convidado conv)
+        {
+            convidados.Remove(convidados.Where(c => c.ConvidadoId == conv.ConvidadoId).First());
+            convidados.Add(conv);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Details(int id)
+        {
+            return View(convidados.Where(m => m.ConvidadoId == id).First());
+        }
+        public IActionResult Delete(int id)
+        {
+            return View(convidados.Where(m => m.ConvidadoId == id).First());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Convidado conv)
+        {
+            convidados.Remove(convidados.Where(c => c.ConvidadoId == conv.ConvidadoId).First());
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ AtividadeAval01/Models/Convidado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AtividadeAval01.Models
+{
+    public class Convidado
+    {
+        public int ConvidadoId { get; set; }
+        public string Name { get; set; }
+        public string EMail { get; set; }
+        public string Telefone { get; set; }
+        public bool? Comparecimento { get; set; }
+    }
+}
